c184f04a6f131fa85bb3f8bb641c568e2f6a2c1e
Faisal Chakiki <faisalchakiki012018@gmail.com>
2023-08-26 17:46:40 +0700

    feat : add navigate at login register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom"
+
 function Login() {
+  const navigate = useNavigate()
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className="lg:w-1/3 md:w-1/2 shadow-lg px-8 py-10 bg-white flex flex-col w-full border h-screen justify-center md:h-fit md:justify-normal">
@@ -13,7 +16,7 @@ function Login() {
           <input type="password" id="password" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
-        <p className="text-gray-500 mt-3 text-center text-xs md:text-lg">Don&apos;t have an account yet? <a href="register" className="text-blue font-semibold">Sign Up</a>.</p>
+        <p className="text-gray-500 mt-3 text-center text-xs md:text-lg">Don&apos;t have an account yet? <a onClick={() => navigate('/register')} className="text-blue font-semibold cursor-pointer">Sign Up</a>.</p>
       </div>
     </div>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,7 @@
+import { useNavigate } from "react-router-dom"
+
 function Register() {
+  const navigate = useNavigate()
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className="lg:w-1/3 md:w-1/2 shadow-lg px-8 py-10 bg-white flex flex-col w-full border h-screen justify-center md:h-fit md:justify-normal">
@@ -21,7 +24,7 @@ function Register() {
           <input type="password" id="password" name="password" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Register</button>
-        <p className="text-gray-500 mt-3 text-center text-xs md:text-lg">Already have an account? <a href="/login" className="text-blue font-semibold">Sign In</a>.</p>
+        <p className="text-gray-500 mt-3 text-center text-xs md:text-lg">Already have an account? <a onClick={() => navigate('/login')} className="text-blue font-semibold cursor-pointer">Sign In</a>.</p>
       </div>
     </div>
   )
